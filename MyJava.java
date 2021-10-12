50015e33ca875ec493dbc3428951f4cc25a90d1b
Sayali1605Shinde <sayalishinde165@gmail.com>
2021-10-12 08:49:34 -0700

    Modified

--- MyJava.java
+++ MyJava.java
@@ -1,6 +1,6 @@
 class MyJava{
 public static void main(String args[])
 {
-        System.out.println(" Git Jenkind Integration");
+        System.out.println(" Git Jenkins Integration.welcome!");
 }
 }
